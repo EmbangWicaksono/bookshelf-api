3e64f41b877cbfe4a7b3b236dd8e78792c095814
EmbangWicaksono <embangwicaksono@gmail.com>
2021-04-30 12:06:54 +0700

    server configuration

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const hapi = require('@hapi/hapi')
+const routes = require('./routes')
+
+const drin = async () => {
+  const server = hapi.server({
+    host: 'localhost',
+    port: 3000
+  })
+
+  server.route({
+    path: '/',
+    method: 'GET',
+    handler: () => 'welcome to hapi'
+  })
+
+  await server.start()
+  console.log('server started at %s', server.info.uri)
+}
+
+process.on('unhandledRejection', (error) => {
+  console.log(error.message)
+  process.exit(1)
+})
+drin()
