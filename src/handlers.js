282bfc550f80c32abe789a6afd9a00020ce8f3bf
EmbangWicaksono <embangwicaksono@gmail.com>
2021-05-01 11:37:40 +0700

    add updateBook handler

--- src/handlers.js
+++ src/handlers.js
@@ -81,4 +81,46 @@ const getBookDetails = (request, h) => {
   return response
 }
 
-module.exports = { addBook, getAllBooks, getBookDetails }
+const updateBook = (request, h) => {
+  const { bookId } = request.params
+  const index = books.findIndex(bookItem => bookItem.id === bookId)
+  const { name, year, author, summary, publisher, pageCount, readPage, reading } = request.payload
+
+  const updatedAt = new Date().toISOString()
+  const finished = (pageCount === readPage)
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount'
+    })
+    response.code(400)
+    return response
+  }
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku'
+    })
+    response.code(400)
+    return response
+  }
+  if (index !== -1) {
+    books[index] = {
+      ...books[index], name, year, author, summary, publisher, pageCount, readPage, reading
+    }
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui'
+    })
+    response.code(200)
+    return response
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Gagal memperbarui buku. Id tidak ditemukan'
+  })
+  response.code(404)
+  return response
+}
+module.exports = { addBook, getAllBooks, getBookDetails, updateBook }
--- src/routes.js
+++ src/routes.js
@@ -15,6 +15,11 @@ const routes = [
     path: '/books/{bookId}',
     method: 'GET',
     handler: handle.getBookDetails
+  },
+  {
+    path: '/books/{bookId}',
+    method: 'PUT',
+    handler: handle.updateBook
   }
 ]
 
